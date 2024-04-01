2cf768a31540d37bcc0c00c594a20dd003c820a1
Renato R <161402218+Nickname5415@users.noreply.github.com>
2024-04-01 17:40:30 -0600

    Add files via upload

---
+++ Discordbot.py
@@ -0,0 +1,79 @@
+import discord
+import random
+import requests
+from discord.ext import commands
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+def get_fox_image_url():
+    url = 'https://randomfox.ca/floof/'
+    res = requests.get(url)
+    data = res.json()
+    return data['image']
+
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.event
+async def on_ready():
+    print(f'Hemos iniciado sesión como {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def goodbye(ctx):
+    await ctx.send(f'Adios, {bot.user}!')
+
+@bot.command()
+async def mem(ctx):
+    XD = random.randint(1,4)
+    if XD == 1:
+        with open('images/mem1.jpg', 'rb') as f:
+         
+            picture = discord.File(f)
+        
+        await ctx.send(file=picture)
+    elif XD == 2:
+        with open('images/mem2.jpg', 'rb') as f:
+         
+            picture = discord.File(f)
+        
+        await ctx.send(file=picture)
+    elif XD == 3:
+        with open('images/mem3.jpg', 'rb') as f:
+         
+            picture = discord.File(f)
+        
+        await ctx.send(file=picture)
+    
+    elif XD == 4:
+        with open('images/mem4.jpg', 'rb') as f:
+         
+            picture = discord.File(f)
+        
+        await ctx.send(file=picture)
+
+@bot.command('duck')
+async def duck(ctx):
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+@bot.command('fox')
+async def fox(ctx):
+    '''Una vez que llamamos al comando fox,
+    el programa llama a la función get_fox_image_url'''
+    image_url = get_fox_image_url()
+    await ctx.send(image_url)
+
+bot.run("MTIxNjkyMjgyNTU3OTk1NDIzNw.GXIbJt.VYiTNnG1RAQgQvCRGtNt9vng1XID4WQNHJ1rB8")
